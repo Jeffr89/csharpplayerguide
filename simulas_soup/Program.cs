10097e297087e2a820bd2a41947b562f2508dc54
Gianfranco Schianchi <gianfranco.schianchi@gmail.com>
2025-02-13 19:25:37 +0100

    Merge del codice locale con i file remoti

---
+++ simulas_soup/Program.cs
@@ -0,0 +1,78 @@
+﻿
+(FoodVariation, MainIngredient, Seasoning) soup;
+
+
+
+soup = (getFoodVariation(), getMainIngredient(), getSeasoning());
+Console.WriteLine($"{soup.Item3} {soup.Item2} {soup.Item1}");
+
+FoodVariation getFoodVariation()
+{
+    while (true)
+    {
+        Console.WriteLine("Select the type soup variation you'd like: (Soup/Stew/Gumbo)");
+        string inputFoodVariation = Console.ReadLine();
+        if (!Enum.TryParse(inputFoodVariation, ignoreCase: true, out FoodVariation foodVariation))
+        {
+            Console.WriteLine("Wrong option selected!");
+            continue;
+        }
+        return foodVariation;
+    }
+}
+
+MainIngredient getMainIngredient()
+{
+    while (true)
+    {
+        Console.WriteLine("Select the main ingredient you'd like: (Mushrooms/Chicken/Carrots/Potatoes)");
+        string inputMainIngredient = Console.ReadLine();
+        if (!Enum.TryParse(inputMainIngredient, ignoreCase: true, out MainIngredient mainIngredient))
+        {
+            Console.WriteLine("Wrong option selected!");
+            continue;
+        }
+        return mainIngredient;
+    }
+}
+
+Seasoning getSeasoning()
+{
+    while (true)
+    {
+        Console.WriteLine("Select the seasoning you'd like: (Spicy/Salty/Sweet)");
+        string inputSeasoning = Console.ReadLine();
+        if (!Enum.TryParse(inputSeasoning, ignoreCase: true, out Seasoning seasoning))
+        {
+            Console.WriteLine("Wrong option selected!");
+            continue;
+        }
+        return seasoning;
+    }
+}
+enum FoodVariation
+{
+    Unknown,
+    Soup,
+    Stew,
+    Gumbo
+}
+
+enum MainIngredient
+{
+    Unknown,
+    Mushrooms,
+    Chicken,
+    Carrots,
+    Potatoes
+}
+
+enum Seasoning
+{
+    Unknown,
+    Spicy,
+    Salty,
+    Sweet
+
+}
+
---
+++ vins_trouble/Program.cs
@@ -0,0 +1,89 @@
+﻿
+
+Arrow arrow = getArrow();
+
+Console.WriteLine($"The arrow costs: {arrow.GetCost()}");
+
+
+
+
+Arrow getArrow()
+{
+    ArrowheadType arrowheadType = GetArrowheadType();
+    FletchingType fletchingType = GetFletchingType();
+    float length = getLenght();
+
+    return new Arrow(arrowheadType, fletchingType, length);
+}
+
+ArrowheadType GetArrowheadType()
+{
+    Console.WriteLine("Pick your arrowhead (steel/wood/obsidian):");
+    string inputArrowhead = Console.ReadLine();
+    Enum.TryParse(inputArrowhead, ignoreCase: true, out ArrowheadType arrowheadType);
+    return arrowheadType;
+}
+
+FletchingType GetFletchingType()
+{
+    Console.WriteLine("Pick your fletching (plastic/turkey/goose):");
+    string inputFletching = Console.ReadLine();
+    Enum.TryParse(inputFletching, ignoreCase: true, out FletchingType fletchingType);
+    return fletchingType;
+
+}
+
+float getLenght()
+{
+    float length = 0;
+    while (length < 60 || length > 100)
+    {
+        Console.WriteLine("Choose the length between 60 and 100; ");
+        length = float.Parse(Console.ReadLine());
+    }
+    return length;
+
+}
+
+
+class Arrow
+{
+    private ArrowheadType _arrowheadType;
+    private FletchingType _fletchingType;
+    private float _length;
+
+    public Arrow(ArrowheadType arrowheadType, FletchingType fletchingType, float length)
+    {
+        _arrowheadType = arrowheadType;
+        _fletchingType = fletchingType;
+        _length = length;
+    }
+
+    public float GetCost()
+    {
+        return (int)_arrowheadType + (int)_fletchingType + _length * 0.05f;
+
+    }
+
+    public ArrowheadType GetArrowheadType() => _arrowheadType;
+
+    public FletchingType GetFletchingType() => _fletchingType;
+    public float GetLength() => _length;
+
+}
+
+
+enum ArrowheadType
+{
+    Steel = 10,
+    Wood = 3,
+    Obsidian = 5
+}
+
+enum FletchingType
+{
+    Plastic = 10,
+    TurkeyFeather = 5,
+    GooseFeather = 3
+}
+
