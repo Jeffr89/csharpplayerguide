46e8d6cbb65be03ba582a5b57bdbcae9c135916a
Gianfranco Schianchi <gianfranco.schianchi@gmail.com>
2025-03-05 15:00:31 +0100

    the feud solution added

---
+++ the_feud/Program.cs
@@ -0,0 +1,24 @@
+﻿using IField;
+using McDroid;
+using IFieldPig = IField.Pig;
+using McDroidPig = McDroid.Pig;
+
+IFieldPig IFieldPig = new IFieldPig();
+McDroidPig McDroid = new McDroidPig();
+Cow McDroidCow = new Cow();
+Sheep IfFieldSheep = new Sheep();
+
+
+namespace IField
+{
+    public class Sheep { };
+
+    public class Pig { };
+}
+
+namespace McDroid
+{
+    public class Cow { };
+    public class Pig { };
+}
+
