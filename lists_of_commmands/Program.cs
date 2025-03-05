7bfb7ebc530050dda63f8ab005f1ef49867a80be
Gianfranco Schianchi <gianfranco.schianchi@gmail.com>
2025-03-05 14:51:21 +0100

    list of commands solution added

---
+++ lists_of_commmands/Program.cs
@@ -0,0 +1,92 @@
+﻿Robot robot = new Robot();
+
+while (true)
+{
+    string? command = Console.ReadLine();
+    if (command == "stop") break;
+
+    robot.Commands.Add(command switch
+    {
+        "on" => new OnCommand(),
+        "off" => new OffCommand(),
+        "north" => new NorthCommand(),
+        "east" => new EastCommand(),
+        "west" => new WestCommand(),
+        "south" => new SouthCommand(),
+        _ => throw new InvalidOperationException("Invalid command")
+    });
+}
+
+
+
+robot.Run();
+public class Robot
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public bool IsPowered { get; set; }
+    public List<IRobotCommand> Commands { get; } = new List<IRobotCommand>();
+    public void Run()
+    {
+        foreach (IRobotCommand command in Commands)
+        {
+            command?.Run(this);
+            Console.WriteLine($"[{X} {Y} {IsPowered}]");
+        }
+    }
+}
+
+public interface IRobotCommand
+{
+    void Run(Robot robot);
+
+}
+
+public class OnCommand : IRobotCommand
+{
+    public void Run(Robot robot)
+    {
+        robot.IsPowered = true;
+    }
+
+}
+
+public class OffCommand : IRobotCommand
+{
+    public void Run(Robot robot)
+    {
+        robot.IsPowered = false;
+    }
+}
+
+public class NorthCommand : IRobotCommand
+{
+    public void Run(Robot robot)
+    {
+        if (robot.IsPowered) robot.Y++;
+    }
+}
+
+public class SouthCommand : IRobotCommand
+{
+    public void Run(Robot robot)
+    {
+        if (robot.IsPowered) robot.Y--;
+    }
+}
+
+public class WestCommand : IRobotCommand
+{
+    public void Run(Robot robot)
+    {
+        if (robot.IsPowered) robot.X--;
+    }
+}
+
+public class EastCommand : IRobotCommand
+{
+    public void Run(Robot robot)
+    {
+        if (robot.IsPowered) robot.X++;
+    }
+}
