695b7a7194d004634e17da9b4f7f8967caecbf66
Jeffry <g.schianchi@hotmail.it>
2025-03-11 17:56:16 +0100

    the long game solution added

---
+++ the_long_game/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Formats.Asn1;
+
+int score = 0;
+ConsoleKey key = ConsoleKey.None;
+Console.WriteLine("What is your name adventurer?");
+string extension = ".txt";
+
+string username = Console.ReadLine();
+
+if (File.Exists(username + extension))
+{
+    score = int.Parse(File.ReadAllText(username + extension));
+}
+while (true)
+{
+    Console.Clear();
+    Console.WriteLine("The key spam begins!");
+    Console.WriteLine($"Score: {score}");
+    key = Console.ReadKey().Key;
+    if (key == ConsoleKey.Enter) break;
+    score++;
+}
+
+File.WriteAllText(username + extension, score.ToString());
+
+// FileStream fileStream = File.Open(username, FileMode.Create);
+// StreamWriter stream = new StreamWriter(fileStream);
+// stream.WriteLine(score);
+// stream.Close();
