ddecc247cce99ee55a14c11efddce80526655342
Jeffry <g.schianchi@hotmail.it>
2025-03-13 15:20:09 +0100

    indexing operand city solution added

---
+++ indexing_operand_city/Program.cs
@@ -0,0 +1,26 @@
+﻿BlockCoordinate blockCoordinate = new BlockCoordinate(4, 3);
+
+Console.WriteLine(blockCoordinate[0]);
+Console.WriteLine(blockCoordinate[1]);
+
+
+public record BlockCoordinate(int Row, int Column)
+{
+
+    public int this[int index]
+    {
+        get
+        {
+            return index switch
+            {
+                0 => Row,
+                1 => Column,
+                _ => throw new IndexOutOfRangeException("Invalid index")
+            };
+
+
+        }
+    }
+
+};
+
