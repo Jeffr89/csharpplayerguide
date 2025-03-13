44be25439876dd77f8826c3621f9bd6c415b6eb2
Gianfranco Schianchi <gianfranco.schianchi@gmail.com>
2025-03-14 00:14:54 +0100

    the repeating stream solution added

---
+++ the_repeating_stream/Program.cs
@@ -0,0 +1,52 @@
+﻿
+Console.WriteLine("Push a key to identify the repeat!");
+RecentNumbers recentNumbers = new RecentNumbers();
+Thread thread = new Thread(NumberGenerator);
+thread.Start(recentNumbers);
+
+while (true)
+{
+    ConsoleKeyInfo consoleKey = Console.ReadKey();
+    if (recentNumbers.IsRepeat()) Console.WriteLine("Correct!");
+    else Console.WriteLine("Wrong");
+
+}
+
+void NumberGenerator(object? obj)
+{
+    Random random = new Random();
+    RecentNumbers recentNumbers = (RecentNumbers)obj;
+    while (true)
+    {
+        int randomNumber = random.Next(10);
+        Console.WriteLine(randomNumber);
+        recentNumbers.NewNumber(randomNumber);
+        Thread.Sleep(500);
+    }
+}
+public class RecentNumbers
+{
+    private readonly object _numberLock = new object();
+
+    private int _previous;
+    private int _current;
+
+    public RecentNumbers()
+    {
+        _previous = -1;
+        _current = -2;
+    }
+
+    public void NewNumber(int num)
+    {
+        lock (_numberLock)
+        {
+            _previous = _current;
+            _current = num;
+        }
+    }
+
+
+    public bool IsRepeat() => _current == _previous;
+
+}
