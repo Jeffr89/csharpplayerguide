3d38b1696155f952713e4a48c2a588fbe9a32fc5
Jeffry <g.schianchi@hotmail.it>
2025-03-18 14:41:28 +0100

    the great humanizer solution added

---
+++ the_great_humanizer/Program.cs
@@ -0,0 +1,8 @@
+﻿
+using Humanizer;
+
+System.Globalization.CultureInfo.DefaultThreadCurrentCulture = new System.Globalization.CultureInfo("en");
+System.Globalization.CultureInfo.DefaultThreadCurrentUICulture = new System.Globalization.CultureInfo("en");
+
+Console.WriteLine($"When is the feast? {DateTime.UtcNow.AddHours(30)}");
+Console.WriteLine($"When is the feast? {DateTime.UtcNow.AddHours(50).Humanize()}");
