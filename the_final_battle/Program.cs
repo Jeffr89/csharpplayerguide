a11b556936414913ed8e93f3a4584cc7c3800c22
Jeffry <g.schianchi@hotmail.it>
2025-03-19 17:26:35 +0100

    The Final Battle - Core Game: The True Programmer

--- the_final_battle/Program.cs
+++ the_final_battle/Program.cs
@@ -1,7 +1,9 @@
 ﻿
 List<Character> heroes = new List<Character>();
 List<Character> monsters = new List<Character>();
-heroes.Add(new Skeleton());
+Console.WriteLine("Tell me your name True Programmer!");
+string input = Console.ReadLine();
+heroes.Add(new TrueProgrammer(input.ToUpper()));
 monsters.Add(new Skeleton());
 Party heroParty = new Party(heroes);
 Party monsterParty = new Party(monsters);
--- the_final_battle/src/Characters/Skeleton.cs
+++ the_final_battle/src/Characters/Skeleton.cs
@@ -6,3 +6,10 @@ public class Skeleton : Character
     }
 }
 
+public class TrueProgrammer : Character
+{
+    public TrueProgrammer(string name) : base(name)
+    {
+        Actions.Add(new SkippingTurnAction());
+    }
+}
